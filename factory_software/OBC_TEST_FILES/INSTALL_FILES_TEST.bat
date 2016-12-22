a5603558b33ba9d5473850f9d1e08c7b469f29ad
Avner Maximov <avner.maximov@micronet-inc.com>
2016-12-22 11:31:28 +0200

    add an IMEI test

--- factory_software/OBC_TEST_FILES/INSTALL_FILES_TEST.bat
+++ factory_software/OBC_TEST_FILES/INSTALL_FILES_TEST.bat
@@ -5,7 +5,9 @@ rem echo          INSTALLING FILES
 rem echo ------------------------------------
 
 ..\adb install -r INSTALL_FILES\nfc_test.apk >nul 2>&1
-rem invoke the nfc test application 
+..\adb install -r INSTALL_FILES\GetIMEI.apk 
+rem invoke android test applications 
+..\adb shell "am start  -n imeiviaadb.micronet.com.imeiviaadb/imeiviaadb.micronet.com.imeiviaadb.MainActivity"
 ..\adb shell "am start  -n 'me.davidvassallo.nfc/me.davidvassallo.nfc.MainActivity' -a android.intent.action.MAIN -c android.intent.category.LAUNCHER" >nul 2>&1
 rem ..\adb push "INSTALL_FILES\MissionImpossible.mp3" /data/local/tmp/ >nul 2>&1
 rem ..\adb push "INSTALL_FILES\track56.mp3" /data/local/tmp/ >nul 2>&1
