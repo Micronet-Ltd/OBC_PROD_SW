24ca5ae92ad81d0554ef3f5a7867f394a18c5255
skrstyen <scott.krstyen@micronet-inc.com>
2017-08-14 16:10:15 -0600

    Made it so it writes ASU value to summary page. Changed default language to english

--- factory_software/OBC_TEST_FILES/Cellular.bat
+++ factory_software/OBC_TEST_FILES/Cellular.bat
@@ -14,16 +14,19 @@ set cell_fail=
 set loop_count=0
 if exist %temp_result% del %temp_result%
 
+rem If language file is not set then default to english
+if not defined language_file set language_file=input/English.txt
+
 set /p line1= <input\cell_input.dat
 for /f "tokens=1,2 delims=:" %%i in ("%line1%") do (
- if %%i EQU lowAsu set lowerBound=%%j
- if %%i EQU highAsu set upperBound=%%j
+ if %%i EQU lowAsu set /A lowerBound=%%j
+ if %%i EQU highAsu set /A upperBound=%%j
 )
 
 for /F "skip=1 delims=" %%i in (input\cell_input.dat) do set "line2=%%i"
 for /f "tokens=1,2 delims=:" %%i in ("%line2%") do (
- if %%i EQU lowAsu set lowerBound=%%j
- if %%i EQU highAsu set upperBound=%%j
+ if %%i EQU lowAsu set /A lowerBound=%%j
+ if %%i EQU highAsu set /A upperBound=%%j
 )
 
 
@@ -34,17 +37,19 @@ rem echo ------------------------------------
 :_test_loop
 ..\adb shell "dumpsys telephony.registry | grep -i signalstrength" > %temp_result%
 for /F "tokens=2" %%G in (%temp_result%) do set /A asuValue=%%G
+
+rem echo ASU = %asuValue%
+
 if %asuValue% EQU 99 (
- set cell_fail=Value is 99.
+ set cell_fail="Value is 99."
  goto _ask_if_retry
 )
-set /A signalRSSI=(%asuValue%*2)-113
 if %lowerBound% GTR %asuValue% (
- set cell_fail=Value (%asuValue%) is greater than %upperBound%
+ set cell_fail=Value %asuValue% is greater than %upperBound%
  goto _ask_if_retry
 )
 if %upperBound% LSS %asuValue% (
- set cell_fail=Value (%asuValue%) is less than than %lowerBound%
+ set cell_fail=Value %asuValue% is less than than %lowerBound%
  goto _ask_if_retry
 )
 goto _test_pass
@@ -65,6 +70,7 @@ set "xprvar="
 for /F "skip=33 delims=" %%i in (%language_file%) do if not defined xprvar set "xprvar=%%i"
 echo ** Cellular %xprvar%
 @echo Cellular test - failed %cell_fail% >> testResults\%result_file_name%.txt
+<nul set /p ".=%asuValue%," >> testResults\summary.csv
 goto :_end_of_file
 
 rem   ############## TEST STATUS ############
@@ -73,6 +79,7 @@ set "xprvar="
 for /F "skip=34 delims=" %%i in (%language_file%) do if not defined xprvar set "xprvar=%%i"
 echo ** Cellular %xprvar%
 @echo Cellular test - passed >> testResults\%result_file_name%.txt
+<nul set /p ".=%asuValue%," >> testResults\summary.csv
 goto _end_of_file
 
 
