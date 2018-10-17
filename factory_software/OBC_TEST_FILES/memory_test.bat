b7b5c186bf0f0c9e5c3f8773e9ef72a8c4170c66
skrstyen <scott.krstyen@micronet-inc.com>
2018-10-17 08:13:29 -0600

    ENH: Updated and added memory test to whole test. Put memory test options in the test options file and added it to the database.

--- factory_software/OBC_TEST_FILES/memory_test.bat
+++ factory_software/OBC_TEST_FILES/memory_test.bat
@@ -16,12 +16,23 @@ rem ----------------------------------------------------------------------------
 
 rem If language file is not set then default to english
 if not defined language_file set language_file=input/English.dat
+if not defined options_file set options_file=input/test_options.dat
 
 set loop_count=0
 set temp_result=tmp.txt
 set mem_total=
 set mem_mapped=
 if exist %temp_result% del %temp_result%
+
+rem Set default values for ram/rom
+set /a expected_rom=8
+set /a expected_ram=1
+
+rem Read in values from test options file
+for /f "tokens=1,2 delims=:" %%i in (%options_file%) do (
+	if /i "%%i" == "ROM" set /a expected_rom=%%j
+	if /i "%%i" == "RAM" set /a expected_ram=%%j
+)
 
 rem get ROM size
 ..\adb shell cat /sys/class/block/mmcblk0/size > %temp_result%
@@ -34,20 +45,20 @@ rem echo ROM size = %rom_size% GB
 
 rem get RAM size
 rem get mem_total
-..\adb shell cat /proc/meminfo | grep MemTotal > %temp_result%
+..\adb shell "cat /proc/meminfo | grep MemTotal" > %temp_result%
 for /F "tokens=2" %%G in (%temp_result%) do set /a mem_total=%%G
 rem echo mem_total %mem_total%
 
 rem get mapped
-..\adb shell cat /proc/meminfo | grep Mapped > %temp_result%
+..\adb shell "cat /proc/meminfo | grep Mapped" > %temp_result%
 for /F "tokens=2" %%G in (%temp_result%) do set /a mem_mapped=%%G
 rem echo mem_mapped %mem_mapped%
 
 set /a ram_size=(%mem_total% + %mem_mapped%)/(1000 * 1024)
 rem echo RAM size = %ram_size% GB
 
-if %rom_size% EQU 8 (
-	if %ram_size% EQU 1 (
+if %rom_size% EQU %expected_rom% (
+	if %ram_size% EQU %expected_ram% (
 		goto _test_pass
 	)
 )
@@ -55,7 +66,7 @@ if %rom_size% EQU 8 (
 :test_fail
 set ERRORLEVEL=1
 call color.bat 0c "** "
-echo echo Memory test - failed ROM = %rom_size% GB and RAM = %ram_size% GB
+echo Memory test - failed ROM = %rom_size% GB and RAM = %ram_size% GB
 @echo Memory test - passed ROM = %rom_size% GB and RAM = %ram_size% GB >> testResults\%result_file_name%.txt
 goto :_end_of_file
 
--- factory_software/OBC_TEST_FILES/unlock.bat
+++ factory_software/OBC_TEST_FILES/unlock.bat
@@ -12,7 +12,7 @@ if exist %temp_result% del %temp_result%
 ::@Echo %so%
 
 @Set so=%so: =%
-..\adb shell am broadcast -a com.micronet.obctestingapp.CHECK_UNLOCK_HASH --es "fs" %so% --es "h" 1448845533 > %temp_result%
+..\adb shell am broadcast -a com.micronet.obctestingapp.CHECK_UNLOCK_HASH --es "fs" %so% --es "h" 1449558848 > %temp_result%
 
 rem Get result code
 set "xprvar="
@@ -30,6 +30,7 @@ if exist %temp_result% del %temp_result%
 goto :eof
 
 :_exit_test
+if exist %temp_result% del %temp_result%
 cd ..
 call :halt 2> nul
 
